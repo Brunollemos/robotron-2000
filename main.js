b5e31dadf2eb00d111973e3405df513274ec888d
Bruno Lemos <brunollemoscontato@gmail.com>
2023-01-30 14:11:05 -0300

    diferentes tipos de função manipulando o DOM

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const robotron = document.querySelector("#robotron");
-robotron.addEventListener("click", dizOi);
 
+// função comum
 function dizOi() {
     console.log("oi");
 }
+
+robotron.addEventListener("click", dizOi);
+
+// função anônima comum
+robotron.addEventListener("click", function () {
+    console.log("Cliquei no robô.");
+});
+
+// arrow function anônima
+robotron.addEventListener("click", () => {
+    console.log("Cliquei no robô");
+});
