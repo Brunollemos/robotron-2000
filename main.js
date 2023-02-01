bc1407bda7b63bf6a9273266d28b1c2fe0b5acea
Bruno Lemos <brunollemoscontato@gmail.com>
2023-02-01 13:58:09 -0300

    Definindo função atualizaEstatisticas

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const controle = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
+console.log(estatisticas);
 const pecas = {
     bracos: {
         forca: 29,
@@ -36,17 +38,26 @@ const pecas = {
 controle.forEach((elemento) => {
     elemento.addEventListener("click", (evento) => {
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
-        console.log(evento.target.parentNode);
+        atualizaEstatisticas(evento.target.dataset.peca);
     });
 });
 
 function manipulaDados(operacao, controle) {
     const peca = controle.querySelector("[data-contador]");
+
     if (operacao === "-") {
         peca.value = parseInt(peca.value) - 1;
     } else {
         peca.value = parseInt(peca.value) + 1;
     }
+}
+
+function atualizaEstatisticas(peca) {
+    console.log(pecas[peca]);
+
+    estatisticas.forEach((elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
+    });
 }
 
 // Rascunho para consulta abaixo
